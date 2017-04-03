4a33189776558440365da171af65331b1d12c4f2
Vladislav Zavjalov <slazav@altlinux.org>
2017-04-03 13:05:00 +0300

    pico_filter/oct/dft_theor.m: more clear code

--- pico_filter/oct/dft_theor.m
+++ pico_filter/oct/dft_theor.m
@@ -1,50 +1,68 @@
 function dft_theor()
 
-  # DFT [ A*exp(-t/t0).*sin(w0*t+p) ] = ?
+  # calculate dft of a signal
+  #   amp*exp(-t/tau).*sin(2*pi*fre*t + ph)
+  function z = dft_calc(amp,tau,fre,ph, t)
+    N = length(t);
+    y = amp*exp(-t/tau).*sin(2*pi*fre*t + ph);
+    z = 2*fft(y)/N;
+  end
 
 
+  # theory for the same thing
+  function z = dft_theor(amp,tau,fre,ph, t)
+    # frequency space
+    N = length(t);
+    T = t(end)-t(1);
+    w = 2*pi*linspace(0,(N-1)/T, N);
+    w0 = 2*pi*fre;
+    w0 = w0*(1 + 1/N); # ?!
+
+    # a complex linear function:
+    lin = (w0-w)*1i - 1/tau;
+
+    # constant factor which depends on boundary conditions:
+    t1 = t(1); t2=t(end);
+    bnd = exp(w0*1i*t2-t2/tau) - exp(w0*1i*t1-t1/tau);
+
+    # phase and amplitude:
+    pha = exp(1i*(ph-pi/2)) * amp/T;
+
+    z = pha*bnd./lin;
+  end
 
   # parameters
-  A  = 0.892;       # signal amplitude
-  n  = 0;           # noise amplitude
-  f0  = 32.18e3;    # frequency
-  w0  = 2*pi*f0;
-  p  = 0.23;        # phase
-  t0 = 0.521;       # tau
+  amp = 0.892;
+  fre = 32.18e3;
+  tau = 0.521;
+  ph  = 0.23;
 
   T  = 1.54;
-  N  = 2^18;      # number of points
+  N  = 1e5;      # number of points
 
-  minf = f0-4;
-  maxf = f0+4;
+  minf = fre-1;
+  maxf = fre+1;
 
 
-  figure; clf; hold on
+  find_figure("dft_theor"); clf; hold on
 
-  for T = 2:0.01:2.03
-
+  # calculate signal for different time grids
+  for T = 2:0.01:2.5
     # build model
     t = linspace(0,T,N);
-    y = A*exp(-t/t0).*sin(w0*t+p) + n*(2*rand(size(t))-1);
-
-    # make fft
     f = linspace(0,(N-1)/T, N);
-    w = 2*pi*f;
-    z = 2*fft(y)/N;
+    z1 = dft_calc(amp,tau,fre,ph, t);
+    z2 = dft_theor(amp,tau,fre,ph, t);
 
     ii = find(f>minf & f<maxf);
-
-    # constant scaling factor which depends on boundary conditions 
-    t1 = 0;
-    t2 = T;
-    w0 = w0*(1 + 1/N);
-    kk = exp(1i*w0*t2 - t2/t0) - exp(1i*w0*t1-t1/t0);
-
-    # analitic function for 1/dft
-    u = ( (w0-w(ii))*1i - 1/t0)/kk * exp(1i*(pi/2-p)) * T/A;
-
-    plot(f(ii), real(z(ii).*u), 'm.-')
-    plot(f(ii), imag(z(ii).*u), 'c.-')
+    if (0)
+      plot(f(ii), real(1./z1(ii)), 'r.-')
+      plot(f(ii), imag(1./z1(ii)), 'b.-')
+      plot(f(ii), zeros(size(ii)), 'k-')
+    else # plot ratio
+      plot(f(ii), real(z1(ii)./z2(ii)), 'm-')
+      plot(f(ii), imag(z1(ii)./z2(ii)), 'c-')
+    end
 
   end
 
