480e65bacab522eae13a589da1f61866623dd45b
Vladislav Zavjalov <slazav@altlinux.org>
2017-05-04 16:22:14 +0300

    sig_filter/test_fit_signal: add noise setting

--- sig_filter/test_fit_signal/test_fit_signal.cpp
+++ sig_filter/test_fit_signal/test_fit_signal.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <iomanip>
 #include <cmath>
+#include <cstdlib>
 #include <complex>
 #include <vector>
 #include <fftw3.h>
@@ -16,6 +17,7 @@ main(){
   double tau = 0.925;
   double amp = 0.582;
   double ph  = 0.1234;
+  double noise = 0.5;
 
   int N1 = 10;
   int N2 = 10000;
@@ -25,13 +27,14 @@ main(){
     int N=N2-N1;
     vector<int16_t> buf(N);
     int max=1<<15;
-    double sc = amp/max;
+    double sc = (amp+noise)/max;
 
     // build a pure sine signal
     for (int i = N1; i<N2; i++){
       double t = i*dt;
-      buf[i-N1] = max*exp(-t/tau)*sin(w0*t + ph);
-    //  buf[i-N1] = max*cos(w0*t);
+      double val = amp*exp(-t/tau)*sin(w0*t + ph) +
+                   noise*(2.0*random()/RAND_MAX-1.0);
+      buf[i-N1] = val/sc;
     }
 
     vector<double> ret = fit_signal(buf.data(), buf.size(), sc, dt, N1*dt);
