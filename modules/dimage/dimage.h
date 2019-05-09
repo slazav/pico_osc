e78d468a72e7a677e455ce1d5412b78d7f80886f
Vladislav Zavjalov <slazav@altlinux.org>
2019-05-09 09:11:15 +0300

    dimage: move some code to dimage.cpp

--- modules/dimage/dimage.h
+++ modules/dimage/dimage.h
@@ -3,7 +3,6 @@
 
 #include <iostream>
 #include <vector>
-#include "rainbow/rainbow.h"
 
 /******************************************************************/
 // Strange double image
@@ -23,42 +22,7 @@ class dImage:std::vector<double> {
   void   set_vs(const double vs_) {vs=vs_;}
 
   void print_pnm(std::ostream & ff,
-        bool uselog = false, const char *grad = "KRYW") const{
-    // find data range (for logscale ignore values <=0)
-    double cmin=+HUGE_VAL;
-    double cmax=-HUGE_VAL;
-    for (int y=0; y<h; y++){
-      for (int x=0; x<w; x++){
-        double v = get(x,y);
-        if (uselog && v<=0) continue;
-        if (uselog) v = log(v);
-        if (v > cmax) cmax = v;
-        if (v < cmin) cmin = v;
-      }
-    }
-    std::cerr << "picture min: " << cmin << " max: " << cmax << "\n";
-    if (amin!=-HUGE_VAL) cmin=amin;
-    if (amax!=+HUGE_VAL) cmax=amax;
-    Rainbow R(cmin, cmax, grad);
-
-    // print data
-    ff << "P6\n" << w+vs << " " << h << "\n255\n";
-    for (int y=0; y<h; y++){
-      for (int x=0; x<w+vs; x++){
-        double v;
-        if (x<w) {
-          v = get(x,y);
-          if (uselog) v = (v<=0)? cmin : log(v);
-        }
-        else {
-          v = cmin + (cmax-cmin)*(h-y-1)/(double)(h-1);
-        }
-        int32_t col = R.get(v);
-        char *cc = (char *)&col;
-        ff << cc[2] << cc[1] << cc[0];
-      }
-    }
-  }
+        bool uselog = false, const char *grad = "KRYW") const;
 };
 
 #endif
